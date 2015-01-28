8aa36c8da8e96c4be30653926c316bab644dbbfc
underchemist <yanns.tremblay@gmail.com>
2015-01-27 17:34:12 -0800

    seem to have working, need to make sure I understand what it is doing though

--- hw2/qmc.cpp
+++ hw2/qmc.cpp
@@ -69,14 +69,92 @@ double elocal(const coord &c1, const coord &c2) {
     return -4.0 + alpha * (d1 + d2 + d3) - d4 / 4.0 + dprod * d2;
 }
 
+void adjust_delta() {
+    delta *= accept_count / (0.5 * N);
+    accept_count = 0;
+}
+
+void mstep(electrons &e) {
+    coord c1(e[0]);
+    coord c2(e[1]);
+    coord t1(DIM, 0.0);
+    coord t2(DIM, 0.0);
+    double w;
+    double E;
+
+    for (size_t i = 0; i < DIM; i++) {
+        t1[i] = c1[i] + delta * (2.0 * rnd_gen() - 1.0);
+        t2[i] = c2[i] + delta * (2.0 * rnd_gen() - 1.0);
+    }
+
+    // move
+    w = psi(t1, t2) / psi(c1, c2);
+    w *= w;
+
+    if (rnd_gen() < w) {
+        for (size_t i = 0; i < DIM; i++) {
+            e[0][i] = t1[i];
+            e[1][i] = t2[i];
+        }
+        ++accept_count;
+    }
+    E = elocal(e[0], e[1]);
+    Esum += E;
+    Esum_sqr += E * E;
+}
+
+void singleMCstep(walkers &r) {
+    for (size_t i = 0; i < N; i++) {
+        mstep(r[i]);
+    }
+}
+
+void MCtherm(walkers &r) {
+    for (int i = 0; i < thermsteps; i++) {
+        singleMCstep(r);
+        adjust_delta();
+    }
+    Esum = 0.0;
+    Esum_sqr = 0.0;
+}
+
+void MCrun(walkers &r) {
+    for (int i = 0; i < MCsteps; i++) {
+        singleMCstep(r);
+        adjust_delta();
+    }
+}
+
 int main() {
     walkers r;
+    double Eave;
+    double Evar;
+    double Err;
+    ofstream f;
+    f.open("data.csv");
+    f << "Eave,Evar,Err,alpha" << endl;
 
     // initialize walkers with positions in [0, 1)
     init(r);
 
-    for (size_t i = 0; i < N; i++) {
-        cout << elocal(r[i][0], r[i][1]) << endl;
+    for (int i = 0; i <= 100; i++) {
+        MCtherm(r);
+
+        MCrun(r);
+
+        Eave = Esum / double(N) / double(MCsteps);
+        Evar = Esum_sqr / double(N) / double(MCsteps) - Eave * Eave;
+        Err = sqrt(Evar) / sqrt(double(N) * double(MCsteps));
+        cout << "alpha = " << alpha
+             << " <E> = " << Eave
+             << " +/- " << Err
+             << " <Evar> = " << Evar << endl;
+
+        f << Eave << "," << Evar << "," << Err << "," << alpha << endl;
+        alpha += 0.005;
+
     }
+    f.close();
+
     return 0;
 }
--- hw2/qmc.hpp
+++ hw2/qmc.hpp
@@ -12,6 +12,7 @@
 using std::cin;
 using std::cout;
 using std::endl;
+using std::ofstream;
 
 using std::vector;
 
@@ -23,14 +24,30 @@ typedef vector<double> coord;
 typedef vector<coord> electrons;
 typedef vector<electrons> walkers;
 
-double alpha = 0.05;  // variational parameter
-double delta;  // trial step size
+double alpha = 0.0;  // variational parameter
+double delta = 1.0;  // trial step size
+double Esum = 0.0;
+double Esum_sqr = 0.0;
+int accept_count = 0;  // number of accepted Metropolis steps
+int MCsteps = 10000;
+int thermsteps = static_cast<int>(MCsteps * 0.2);
 
 // function prototypes
 void init(walkers &r);
 
 double psi(const coord &c1, const coord &c2);
 
+double elocal(const coord &c1, const coord &c2);
+
+void adjust_delta();
+
+void mstep(electrons &e);
+
+void singleMCstep(walkers &r);
+
+void MCtherm(walkers &r);
+
+void MCrun(walkers &r);
 
 
 #endif
